6da4aefe87f69c10035a6653ff130f05faa8e3bd
Tony Thesslund <e2101348@edu.vamk.fi>
2026-03-17 17:17:27 +0200

    chore: initialize Vite React TS starter with Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+     <h1 className="text-2xl font-bold mb-8 text-slate-700">Welcome to React!</h1>
+    </div>
+  )
+}
+export default App
