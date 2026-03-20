249967416be51653b7a94eee493f1c80f5aef757
TonyThesslund <tonythesslund@gmail.com>
2026-03-20 14:37:30 +0200

    feat: Task 1.5: Top Bar (Header)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Header } from "./components/Header"
+
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 p-8">
+        <Header/>
      <h1 className="text-2xl font-bold mb-8 text-slate-700">Welcome to React!</h1>
     </div>
-  )
+  );
 }
 export default App
