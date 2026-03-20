58feb9bf51bd73f216a5aa5d190e49d411430da4
S4mppaaa <e2001003@edu.vamk.fi>
2026-03-20 14:40:41 +0200

    Task 1.6: Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { Footer } from './Components/Footer';
+
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 p-8">
-     <h1 className="text-2xl font-bold mb-8 text-slate-700">Welcome to React!</h1>
+    <div className="min-h-screen bg-gray-100 p-8 flex flex-col justify-between">
+      
+      <h1 className="text-2xl font-bold mb-8 text-slate-700">
+        Welcome to React!
+      </h1>
+
+      <Footer/>
     </div>
-  )
+  );
 }
-export default App
+
+export default App;
---
+++ src/Components/Footer.tsx
@@ -0,0 +1,22 @@
+export function Footer() {
+  return (
+    <footer className="bg-[#A2D135] text-black p-8 mt-12 w-full flex flex-wrap justify-around items-start">
+      
+      {/* Column 1: Contact */}
+      <div>
+        <h2 className="font-bold mb-4">Contact</h2>
+      </div>
+
+      {/* Column 2: Social Media */}
+      <div>
+        <h2 className="font-bold mb-4">Social Media</h2>
+      </div>
+
+      {/* Column 3: Branding */}
+      <div>
+        <h2 className="font-bold mb-4">Branding</h2>
+      </div>
+
+    </footer>
+  );
+}
