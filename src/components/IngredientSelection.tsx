be90ed5f64b5837aa9f3a06c64f1ad1b6e30747b
Tony Thesslund <e2101348@edu.vamk.fi>
2026-03-21 09:06:49 +0200

    Task 1.11: Ingredient Panel (IngredientSection Shell)

---
+++ src/components/IngredientSelection.tsx
@@ -0,0 +1,11 @@
+export function IngredientSelection() {
+    return (
+        <div className="bg-zinc-800 rounded-[3rem] p-8 text-white w-full shadow-lg">
+            <input type="text" className="rounded-full px-6 py-3 text-black outline-none w-64 border-2 border-transparent focus:border-[#A2D135]"/>
+
+            <button className="bg-[#A2D135] text-black font-bold px-6 py-2 rounded-full">Placeholder</button>
+            <button className="bg-[#A2D135] text-black font-bold px-6 py-2 rounded-full">Placeholder</button>
+            <button className="bg-[#A2D135] text-black font-bold px-6 py-2 rounded-full">Placeholder</button>
+        </div>
+    )
+}
