f2a51edc91921b6217f253615155d0ba7a1c4f87
ilarikunnari <ilari.kunnari@gmail.com>
2026-04-01 23:03:59 +0200

    Task 2.8: Fetching Ingredients

--- src/pages/Configurator.tsx
+++ src/pages/Configurator.tsx
@@ -1,7 +1,5 @@
-
 import { useEffect, useState } from "react";
-import { getBowls, getCategories } from "../services/api";
-import { getBowls } from "../services/getBowls";
+import { getBowls, getCategories, getIngredients } from "../services/api";
 
 import { BaseSelection } from "../components/BaseSelection";
 import { BowlSelection } from "../components/BowlSelection";
@@ -36,7 +34,7 @@ export default function Configurator() {
   const [isLoadingCategories, setIsLoadingCategories] = useState(true);
   const [categoriesError, setCategoriesError] = useState<string | null>(null);
 
-  useEffect(() => {
+useEffect(() => {
     async function fetchBowls() {
       try {
         const data = await getBowls();
@@ -47,10 +45,7 @@ export default function Configurator() {
     }
 
     fetchBowls();
-}, []);
-  
 
-  useEffect(() => {
     const fetchCategories = async () => {
       try {
         const data = await getCategories<Category[]>();
@@ -66,19 +61,33 @@ export default function Configurator() {
     fetchCategories();
 
   }, []);
+
+  useEffect(() => {
+    const fetchIngredients = async () => {
+      try {
+        const data = await getIngredients<Ingredient[]>();
+        setIngredients(data);
+      } catch (err) {
+        console.error("Failed to fetch ingredients:", err);
+      }
+    };
+
+    fetchIngredients();
+  }, []);
   
 
   return (
     <div className="flex-1 max-w-6xl w-full mx-auto p-6 flex flex-col gap-8 mt-4">
       <div className="flex">
 
-      {isLoadingCategories && <p>Loading categories...</p>}
-      {categoriesError && <p className="text-red-500">{categoriesError}</p>}
+        {isLoadingCategories && <p>Loading categories...</p>}
+        {categoriesError && <p className="text-red-500">{categoriesError}</p>}
 
         <BowlSelection bowls={bowls}/>
         <CenterBowl />
         <BaseSelection />
       </div>
+
       <IngredientSelection categories={categories} ingredients={ingredients}/>
       <SummaryBar />
     </div>
