6d514b88696ccf9e85af9c4d506a9f96686283d0
S4mppaaa <e2001003@edu.vamk.fi>
2026-04-01 19:31:45 +0300

    Task 2.3: Connecting navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
-import { BaseSelection } from "./components/BaseSelection";
-import { BowlSelection } from "./components/BowlSelection";
-import { CenterBowl } from "./components/CenterBowl";
-import { IngredientSelection } from "./components/IngredientSelection";
-import { SummaryBar} from "./components/SummaryBar";
+
+import Configurator from "./pages/Configurator";
+import Community from "./pages/Community";
+import Print from "./pages/Print";
 
 function App() {
   return (
@@ -12,13 +13,11 @@ function App() {
       <Header/>
 
       <main className="flex-1 max-w-6xl w-full mx-auto p-6 flex flex-col gap-8 mt-4">
-        <div className="flex">
-          <BowlSelection/>
-          <CenterBowl/>
-          <BaseSelection/>
-        </div>
-        <IngredientSelection/>
-        <SummaryBar/>
+        <Routes>
+          <Route path="/" element={<Configurator/>} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/print" element={<Print />} />
+        </Routes>
       </main>
 
       <Footer/>
