430edbf60bc34f8e7b734970cae49913bf803f54
S4mppaaa <e2001003@edu.vamk.fi>
2026-04-01 19:03:59 +0300

    Task 2.6: Fetching Bowls

--- src/pages/Configurator.tsx
+++ src/pages/Configurator.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import { getBowls } from "../services/getBowls";
+
 import { BaseSelection } from "../components/BaseSelection";
 import { BowlSelection } from "../components/BowlSelection";
 import { CenterBowl } from "../components/CenterBowl";
@@ -5,6 +8,21 @@ import { IngredientSelection } from "../components/IngredientSelection";
 import { SummaryBar } from "../components/SummaryBar";
 
 export default function Configurator() {
+  const [bowls, setBowls] = useState([]);
+
+  useEffect(() => {
+    async function fetchBowls() {
+      try {
+        const data = await getBowls();
+        setBowls(data);
+      } catch (error) {
+        console.error("Failed to fetch bowls:", error);
+      }
+    }
+
+    fetchBowls();
+  }, []);
+
   return (
     <div className="flex-1 max-w-6xl w-full mx-auto p-6 flex flex-col gap-8 mt-4">
       <div className="flex">
